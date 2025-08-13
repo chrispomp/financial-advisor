772bb98067d8da26006fcd7d48414661c9e4be9e
chrispomp <christopher.pomponio@gmail.com>
2025-08-13 17:57:12 -0400

    fix

--- financial_advisor/sub_agents/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Expose sub-agents at the package level."""
-
-from .market_analyst import market_analyst
-from .portfolio_analyst import portfolio_analyst
-
-__all__ = ["market_analyst", "portfolio_analyst"]
